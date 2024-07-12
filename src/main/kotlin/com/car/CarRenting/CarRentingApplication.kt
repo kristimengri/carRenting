02e5be80288621a85f48349b51a0fe22795c5365
kristimengri <kristimengri@gmail.com>
2024-07-12 10:56:53 +0200

    deploy test

--- src/main/kotlin/com/car/CarRenting/CarRentingApplication.kt
+++ src/main/kotlin/com/car/CarRenting/CarRentingApplication.kt
@@ -4,16 +4,26 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import org.springframework.scheduling.annotation.EnableAsync
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditorAware")
 @EnableAsync
 @EnableWebMvc
-class CarRentingApplication
+class CarRentingApplication {
 
-fun main(args: Array<String>) {
-    runApplication<CarRentingApplication>(*args)
+    @GetMapping("/testMessage")
+    fun testMessage(): String{
+        return "Testing Deploy"
+    }
+
+
+    fun main(args: Array<String>) {
+        runApplication<CarRentingApplication>(*args)
+    }
+
+
 }
 
 
